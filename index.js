6f1bd0eb39a383858f3d74358522f45919233595
PabloAtanes <144136968+PabloAtanes@users.noreply.github.com>
2025-02-04 13:36:38 +0100

    re-installed prisma now only error is no permission to create database

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const users = await prisma.user.findMany();
+    console.log(users);
+}
+
+main()
+    .catch(e => {
+        throw e;
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
