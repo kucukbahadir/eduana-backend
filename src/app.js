65410e07a2aaad78d8842bacc25c326bb75d42ff
jay <junemassa@gmail.com>
2025-02-07 13:57:06 +0100

    database setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
-const express = require('express');
-const healthCheckerRouter = require('./routes/healthchecker');
+import express from 'express';
+import authRoutes from './routes/auth.js';
+import adminAuthMiddleware from './middleware/adminAuth.js';
+import healthCheckerRouter from './routes/healthchecker';
 
 const app = express();
+
 app.use(express.json());
 app.use('/api', healthCheckerRouter);
+app.use('/api/users', adminAuthMiddleware, authRoutes);
 
-module.exports = app;
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    message: 'Something went wrong!',
+    error: err.message
+  });
+});
+
+export default app;
