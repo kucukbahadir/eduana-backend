7984d0a666dd74b17fbb69517c60e0dd380e39b8
jay <junemassa@gmail.com>
2025-02-11 17:12:33 +0100

    re-installed cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const authRoutes = require('./routes/auth.js');
 const adminAuthMiddleware = require('../middleware/adminAuth.js');
 const healthCheckerRouter = require('./routes/healthchecker.js');
 
 const app = express();
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+// Allow preflight requests
+app.options('*', cors());
 
 app.use(express.json());
 app.use('/api', healthCheckerRouter);
