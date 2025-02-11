6e955d714c0318ff80c19e576b256e9ff4bb572b
PabloAtanes <144136968+PabloAtanes@users.noreply.github.com>
2025-02-11 12:08:18 +0100

    added router.post to controller

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,19 +1,7 @@
-import prisma from "@prisma/client";
+import AdminService from '../sevices/adminService';
+const express = require('express');
+const router = express.Router();
 
-class AdminController {
+const adminService = new AdminService
 
-    // make a changeUserRole function that changes the old role with the new one using the users userid using prisma
-    async changeUserRole(req, res) {
-        try {
-            const {newRole, userId} = req.body;
-            const updatedUser = await prisma.user.update({
-                where: {id: userId},
-                data: {role: newRole},
-            });
-            res.status(200).json(updatedUser);
-        } catch (err) {
-            console.log(err);
-            res.status(500).send(err);
-        }
-    }
-}
+router.post('/role', (req, res) => adminService.changeUserRole(req ,res));
