9076938bb6196e1125edd95df29762756fb4247f
PabloAtanes <144136968+PabloAtanes@users.noreply.github.com>
2025-02-12 10:23:50 +0100

    added a logger file

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const healthCheckerRouter = require('./routes/healthchecker');
-const adminRouter = require('./routes/adminRoutes');
+const express = require("express");
+const healthCheckerRouter = require("./routes/healthchecker");
+const userRouter = require("./routes/userRoutes");
+const adminRouter = require("./routes/adminRoutes");
 
 const app = express();
 app.use(express.json());
-app.use('/api', healthCheckerRouter);
-app.use('/api/admin', adminRouter)
-
-module.exports = app;
+app.use("/api", healthCheckerRouter);
+app.use("/api/users", userRouter);
+app.use("/api/admin", adminRouter);
