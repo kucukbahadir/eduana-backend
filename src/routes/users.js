116f16771ddf8893ded28d3d8ab1e974fea71e7e
j9mmy <127404933+j9mmy@users.noreply.github.com>
2025-02-12 10:13:54 +0100

    refactor: update password reset email to include clickable link

--- src/routes/users.js
+++ src/routes/users.js
@@ -62,7 +62,7 @@ router.post('/reset-password/', async (req, res) => {
                 <h1>Password Reset</h1>
                 <p>You are receiving this email because you (or someone else) have requested the reset of the password for your account.</p>
                 <p>Please click on the following link, or paste this into your browser to complete the process:</p>
-                <strong>${url}/api/users/update-password/${token}</strong>
+                <a href="${url}/api/users/update-password/${token}">${url}/api/users/update-password/${token}</a>
                 <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>
             `
         };
