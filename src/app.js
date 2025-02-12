0e943be43d7655ce9eb57146c7084f9f791b688c
PabloAtanes <144136968+PabloAtanes@users.noreply.github.com>
2025-02-12 13:15:17 +0100

    made small additions after feedback

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const healthCheckerRouter = require("./routes/healthchecker");
-// const userRouter = require("./routes/userRoutes");
 const adminRouter = require("./routes/adminRoutes");
 
 const app = express();
 app.use(express.json());
 app.use("/api", healthCheckerRouter);
-//app.use("/api/users", userRouter);
 app.use("/api/admin", adminRouter);
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const AdminController = require("../controllers/adminController");
+/*
+this const's will be usable when the jwt is implemented
 const { authenticate, authorizeAdmin } = require("../middleware/authMiddleware");
-
+*/
 const router = express.Router();
-router.put("/:id/role", authenticate, authorizeAdmin, (req, res) => AdminController.changeUserRole(req, res));
+router.put("/:id/role", (req, res) => AdminController.changeUserRole(req, res));
 
 module.exports = router;
