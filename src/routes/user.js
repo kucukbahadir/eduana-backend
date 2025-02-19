b8d92fe479bd0fc8767c27215250e34793ed6d7c
j9mmy <127404933+j9mmy@users.noreply.github.com>
2025-02-19 15:59:16 +0100

    refactor: misc merge conflicts

--- src/routes/user.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const UserController = require('../controllers/userController');
-
-const router = express.Router();
-
-router.get('/users', UserController.getUserDetails);
-
-module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
 
+router.get('/', (req, res) => UserController.getUserDetails(req, res));
 router.post('/reset-password', (req, res) => UserController.requestPasswordReset(req, res));
 router.put('/update-password/:token', (req, res) => UserController.changePassword(req, res));
 
