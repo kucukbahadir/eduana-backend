c906372fd57f1aa80f3e09ed4ce65acf14ae9646
PabloAtanes <144136968+PabloAtanes@users.noreply.github.com>
2025-03-12 13:41:12 +0100

    made the function in controller

--- src/controllers/studentController.js
+++ src/controllers/studentController.js
@@ -4,19 +4,39 @@ class StudentController {
             const classId = req.params.classId;
 
             if (!classId) {
-                return res.status(400).json({ error: 'classId is required' });
+                return res.status(400).json({error: 'classId is required'});
             }
 
             const students = await StudentService.getStudentsByClassId(classId);
 
             if (!students.length) {
-                return res.status(404).json({ error: 'No students found or class does not exist' });
+                return res.status(404).json({error: 'No students found or class does not exist'});
             }
 
             return res.status(200).json(students);
         } catch (err) {
             console.error("Error fetching students:", err);
-            return res.status(500).json({ error: 'Internal server error' });
+            return res.status(500).json({error: 'Internal server error'});
+        }
+    }
+
+    async getStudentsByTeacherId(req, res) {
+        try {
+            const teacherId = req.params.teacherId;
+
+            if (!teacherId) {
+                return res.status(400).json({error: 'teacherId is required'})
+            }
+            const students = await StudentService.getStudentsByteacherId(teacherId);
+
+            if (!students.length) {
+                return res.status(404).json({error: 'No students found or teacher does not exist'});
+            }
+
+            return res.status(200).json(students);
+        } catch (err) {
+            console.error("Error fetching students:", err);
+            return res.status(500).json({error: 'Internal server error'});
         }
     }
 }
