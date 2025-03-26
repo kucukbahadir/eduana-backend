0e552cb42936f8ef2aa7232d546c24baadda09c5
PabloAtanes <144136968+PabloAtanes@users.noreply.github.com>
2025-03-26 13:15:45 +0100

    made the route

--- src/routes/teachers.js
+++ src/routes/teachers.js
@@ -6,4 +6,5 @@ const router = express.Router();
 router.get("/:teacherId/classes", (req, res) => TeacherController.getClasses(req, res));
 router.get("/:teacherId/students", (req, res) => TeacherController.getStudents(req, res));
 
+router.post("/class/evaluation", (req, res) => {TeacherController.postEvaluation(req, res)});
 module.exports = router;
