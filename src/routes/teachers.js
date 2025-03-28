74e5f72101a0ee1829156f9c301fbd086ad6e293
PabloAtanes <144136968+PabloAtanes@users.noreply.github.com>
2025-03-28 12:00:30 +0100

    made the api

--- src/routes/teachers.js
+++ src/routes/teachers.js
@@ -5,5 +5,5 @@ const router = express.Router();
 
 router.get("/:teacherId/classes", (req, res) => TeacherController.getClasses(req, res));
 router.get("/:teacherId/students", (req, res) => TeacherController.getStudents(req, res));
-
+router.get("/:teacherId/course/:courseId", (req, res) => TeacherController.getCourseInfoById(req, res));
 module.exports = router;
