28cd7d3cc04f76be5242acc7022034ec15b13011
PabloAtanes <144136968+PabloAtanes@users.noreply.github.com>
2025-04-15 11:58:17 +0200

    implemented the RBAC

--- src/routes/teachers.js
+++ src/routes/teachers.js
@@ -6,9 +6,9 @@ const { hasRole } = require("../middelware/rbacMiddlewares");
 
 const router = express.Router();
 
-router.get("/:teacherId/classes", authenticateUser, hasRole(["ADMIN", "TEACHER"]), (req, res) => TeacherController.getClasses(req, res));
-router.get("/:teacherId/students", authenticateUser, hasRole(["ADMIN", "TEACHER"]), (req, res) => TeacherController.getStudents(req, res));
-router.get("/:teacherId/courses",( req, res) => TeacherController.getAllCourses(req, res));
+router.get("/:teacherId/classes", authenticateUser,hasRole( "TEACHER"), (req, res) => TeacherController.getClasses(req, res));
+router.get("/:teacherId/students", authenticateUser,hasRole( "TEACHER"), (req, res) => TeacherController.getStudents(req, res));
+router.get("/:teacherId/courses",authenticateUser, hasRole( "TEACHER"), ( req, res) => TeacherController.getAllCourses(req, res));
 
-router.post("/:teacherId/sessions/:sessionId/evaluation", (req, res) => {EvaluationController.submitEvaluation(req, res)});
+router.post("/:teacherId/sessions/:sessionId/evaluation",authenticateUser, hasRole( "TEACHER"), (req, res) => {EvaluationController.submitEvaluation(req, res)});
 module.exports = router;
